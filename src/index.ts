2ebffdaf9c84b25f26acbd1bf2f7735d358bbbd1
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2023-05-29 15:06:41 +0300

    use account gql module

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,24 @@ import * as path from "path";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+import { gqlModule as accountGqlModule } from "./modules/account/gql";
+import { GqlModule } from "./modules/gqlModule";
+
+const gqlModules: GqlModule[] = [accountGqlModule];
+
+const mergedResolvers = [...gqlModules.map((x) => x.resolvers)];
+
 const typeDefs = readFileSync(
   path.join(__dirname, "./schema.generated.graphql"),
   "utf-8"
 );
-
-const viewer = {
-  id: 1,
-  name: "Random User",
-  email: "random@random.org",
-};
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    viewer: () => viewer,
-  },
-};
 
 const start = async () => {
   // The ApolloServer constructor requires two parameters: your schema
   // definition and your set of resolvers.
   const server = new ApolloServer({
     typeDefs,
-    resolvers,
+    resolvers: mergedResolvers,
   });
 
   // Passing an ApolloServer instance to the `startStandaloneServer` function:
