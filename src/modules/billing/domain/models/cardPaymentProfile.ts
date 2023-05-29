8117dff54073cf9685f90e6e58f5491b94df4db6
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2023-05-29 20:33:47 +0300

    add card payment profile model (entity)

---
+++ src/modules/billing/domain/models/cardPaymentProfile.ts
@@ -0,0 +1,38 @@
+interface CardPaymentProfileProps {
+  firstName: string;
+  lastName: string;
+}
+
+export class CardPaymentProfile {
+  #id: string;
+  #props: CardPaymentProfileProps;
+
+  constructor(props: CardPaymentProfileProps, id: string) {
+    this.#id = id;
+    this.#props = props;
+  }
+
+  get id(): string {
+    return this.#id;
+  }
+
+  get firstName() {
+    return this.#props.firstName;
+  }
+
+  set firstName(value: string) {
+    this.#props.firstName = value;
+  }
+
+  get lastName() {
+    return this.#props.lastName;
+  }
+
+  set lastName(value: string) {
+    this.#props.lastName = value;
+  }
+
+  static create(props: CardPaymentProfileProps, id: string) {
+    return new CardPaymentProfile(props, id);
+  }
+}
