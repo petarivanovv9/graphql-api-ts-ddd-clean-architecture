43a9898c00749cb0a91eafb8d407399634349cca
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2023-05-29 14:38:14 +0300

    use generated schema in server

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,27 @@
+import { readFileSync } from "fs";
+import * as path from "path";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const typeDefs = readFileSync(
+  path.join(__dirname, "./schema.generated.graphql"),
+  "utf-8"
+);
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
+const viewer = {
+  id: 1,
+  name: "Random User",
+  email: "random@random.org",
+};
 
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    viewer: () => viewer,
   },
 };
 
