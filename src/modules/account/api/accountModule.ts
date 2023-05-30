a9638f48ba713f143b4c0b78a5cb0083ef0c073c
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2023-05-30 17:24:13 +0300

    add account service

--- src/modules/account/api/accountModule.ts
+++ src/modules/account/api/accountModule.ts
@@ -1,4 +1,5 @@
-import { UserDto } from './types';
+import { AccountService } from '../domain/services/accountService';
+import { AccountDto, UserDto } from './types';
 
 export class AccountModule {
   getUserById(id: number): Promise<UserDto | undefined> {
@@ -8,5 +9,10 @@ export class AccountModule {
     console.log('calling `getUserById` with `id` ', id);
 
     return Promise.resolve(undefined);
+  }
+
+  getAccountByCompany(company: string): Promise<AccountDto | undefined> {
+    const accountService = new AccountService();
+    return accountService.getAccountByCompany(company);
   }
 }
---
+++ src/modules/account/domain/services/accountService.ts
@@ -0,0 +1,10 @@
+export class AccountService {
+  async getAccountByCompany(company: string) {
+    // TODO:
+    // >>> IMPLEMENT
+
+    console.log('calling `getAccountByCompany` with `company` ', company);
+
+    return Promise.resolve(undefined);
+  }
+}
