e725de86a3590780b14d96f898df1de22bcc9947
Petar Ivaylov Ivanov <pepincho95@gmail.com>
2023-06-05 14:08:15 +0300

    add test folder

---
+++ src/tests/billing/queries/cardPaymentProfileQueries.spec.ts
@@ -0,0 +1,21 @@
+//
+// Integration tests for the CardPaymentProfile related queries
+//
+
+// describe('CardPaymentProfiles by Account', () => {
+//   it('should return response when successful', async () => {
+//     //
+//     // TODO: write test
+//     // - setup test data
+//     // - call query:
+//     // viewer {
+//     //     account {
+//     //         cardPaymentProfiles {
+//     //             ...
+//     //             ...
+//     //         }
+//     //     }
+//     // }
+//     // - assert the result
+//   });
+// });
